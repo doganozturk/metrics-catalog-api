68d727adeb0a7d259c862f0b03ccc031628ec206
Doğan Öztürk <doganozturk2005@gmail.com>
2020-05-02 01:42:54 +0300

    Add .env support.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MetricsModule } from './metrics/metrics.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
     imports: [
-        MongooseModule.forRoot('mongodb://localhost/metrics-catalog', {
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(process.env.DB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         }),
